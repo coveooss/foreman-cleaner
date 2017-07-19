d57779923a4bee5e3a1a578ef109cf56dbc115cd
Thomas De Giacinto <tdegiacinto@coveo.com>
2017-07-19 14:46:47 +0200

    correct entryppoint

--- files/install/entrypoint.sh
+++ files/install/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -e
+
 # Create Certificate
 puppet agent --noop --server=$FOREMANPROXY_HOST
 
 # Add cron for clean
 echo "* */1 * * * python -W ignore /install/host-cleaner.py clean_old_host > /var/log/cron.log" >> /var/spool/cron/crontabs/root
 service cron start
+
+exec "$@"
