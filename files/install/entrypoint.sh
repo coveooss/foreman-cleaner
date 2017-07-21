4ce62015d470da7fc929d12c3177509021acc3d6
Thomas De Giacinto <tdegiacinto@coveo.com>
2017-07-21 12:52:00 +0200

    add env variable in cron

--- files/install/entrypoint.sh
+++ files/install/entrypoint.sh
@@ -5,8 +5,12 @@ set -e
 # Create Certificate
 puppet agent --noop --server=$FOREMANPROXY_HOST
 
+# Get env variable for cronjob
+env | grep FOREMAN | sed 's/^\(.*\)$/export \1/g' > /root/envs.sh
+chmod +x /root/envs.sh
+
 # Add cron for clean
-echo "0 * * * * python -W ignore /install/host-cleaner.py clean_old_host > /var/log/cron.log" >> /var/spool/cron/crontabs/root
-service cron start
+echo "0 * * * * . /root/envs.sh; python -W ignore /install/host-cleaner.py clean_old_host >> /var/log/cron.log" >> /var/spool/cron/crontabs/root
+service cron restart
 
 exec "$@"
--- files/install/host-cleaner.py
+++ files/install/host-cleaner.py
@@ -4,7 +4,6 @@ import os
 import requests
 import socket
 from foreman.client import Foreman
-from subprocess import call
 
 
 class ForemanProxy(object):
@@ -17,7 +16,7 @@ class ForemanProxy(object):
 
         self.session.headers.update(
         {
-            'Accept': 'application/json; version=1.15',
+            'Accept': 'application/json',
             'Content-type': 'application/json',
         })
 
@@ -32,6 +31,55 @@ class ForemanProxy(object):
         else:
             print('{} deleted'.format(host))
         return r
+
+    def get_certificates(self):
+        uri = "/puppet/ca"
+        r = self.session.get(self.url + uri)
+        if r.status_code < 200 or r.status_code >= 300:
+            print('Something went wrong: %s' % r.text)
+        else:
+            return r.json()
+
+
+@baker.command()
+def clean_old_certificates():
+    # Retrieve config from ENV
+    foreman_url = os.environ.get('FOREMAN_URL')
+    foreman_user = os.environ.get('FOREMAN_USER')
+    foreman_password = os.environ.get('FOREMAN_PASSWORD')
+    foreman_proxy_url = "https://{}:{}".format(os.environ.get('FOREMANPROXY_HOST'), os.getenv('FOREMANPROXY_PORT','8443'))
+
+    # connect to Foreman and ForemanProxy
+    f=Foreman(foreman_url, (foreman_user, foreman_password))
+    fp = ForemanProxy(foreman_proxy_url)
+
+    # Build a certificates list with only hostcert, discarding specific certs used for foreman, puppet, etc ...
+    host_pattern = ['ndev', 'nsta', 'nifd', 'npra', 'nifp', 'nhip', 'nifh', 'win']
+    certs = fp.get_certificates().keys()
+    certs = [cert for cert in certs if any(pattern in cert for pattern in host_pattern)]
+    foreman_hosts = []
+
+    # Get all host in foreman
+    get_next_page = True
+    page = 1
+    while get_next_page:
+        result = f.index_hosts(per_page="1000", page=str(page))
+        if len(result) == 1000:
+            page += 1
+        else:
+            get_next_page = False
+        for host in result:
+            foreman_hosts.append(host["host"]["name"])
+
+    certs_to_delete = list(set(certs) - set(foreman_hosts))
+
+    for cert in certs_to_delete:
+        print(" {} will be deleted".format(cert))
+        try:
+            fp.delete_certificate(cert)
+        except:
+            print(" {} couldn't be deleted".format(cert))
+
 
 @baker.command()
 def clean_old_host():
