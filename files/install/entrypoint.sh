2e5fc4baa9a72ea7d0a0bcdbb2cbca90415665a1
Thomas De Giacinto <tdegiacinto@coveo.com>
2017-09-14 08:43:58 +0200

    Change cron output

--- files/install/entrypoint.sh
+++ files/install/entrypoint.sh
@@ -8,6 +8,6 @@ env | grep FOREMAN | sed 's/^\(.*\)$/export \1/g' > /root/envs.sh
 chmod +x /root/envs.sh
 
 # Add cron for clean
-echo "0 * * * * root . /root/envs.sh; /usr/bin/python -W ignore /install/host-cleaner.py clean_old_host >> /var/log/cron.log" >> /etc/cron.d/foreman-cleaner
+echo "0 * * * * root . /root/envs.sh; /usr/bin/python -W ignore /install/host-cleaner.py clean_old_host > /proc/1/fd/1" >> /etc/cron.d/foreman-cleaner
 
 service cron restart && tail -f /var/log/cron.log
