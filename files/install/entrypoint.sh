9e46b6c4cbcd1695c3b1e6496f2ff7f030be4c87
Thomas <tdegiacinto@coveo.com>
2019-02-19 10:01:20 +0100

    fix broken function call
    improve ds cleaning

--- files/install/entrypoint.sh
+++ files/install/entrypoint.sh
@@ -8,11 +8,11 @@ env | grep -E 'AWS|FOREMAN|DS|LDAP|COMPUTER_DN' | sed 's/^\(.*\)$/export \1/g' >
 chmod +x /root/envs.sh
 
 # Add cron for clean
-echo "0 * * * * root . /root/envs.sh; /usr/bin/python -W ignore /install/host-cleaner.py clean_old_host > /proc/1/fd/1" >> /etc/cron.d/foreman-cleaner
+echo "0 * * * * root . /root/envs.sh; /usr/bin/python -W ignore /install/host-cleaner.py clean-old-host > /proc/1/fd/1" >> /etc/cron.d/foreman-cleaner
 # Add cron to clean all old certificates 
-echo "30 11 * * * root . /root/envs.sh; /usr/bin/python -W ignore /install/host-cleaner.py clean_old_certificates > /proc/1/fd/1" >> /etc/cron.d/foreman-cleaner
+echo "30 11 * * * root . /root/envs.sh; /usr/bin/python -W ignore /install/host-cleaner.py clean-old-certificates > /proc/1/fd/1" >> /etc/cron.d/foreman-cleaner
 # Add cron to clean DS leftovers
-echo "30 6 * * * root . /root/envs.sh; /usr/bin/python -W ignore /install/host-cleaner.py clean_ds > /proc/1/fd/1" >> /etc/cron.d/foreman-cleaner
+echo "30 6 * * * root . /root/envs.sh; /usr/bin/python -W ignore /install/host-cleaner.py clean-ds > /proc/1/fd/1" >> /etc/cron.d/foreman-cleaner
 # Add cron to autoheal approbation relation between windows and DS
-echo "30 * * * * root . /root/envs.sh; /usr/bin/python -W ignore /install/check_windows.py check_join -c /install/config.yaml > /proc/1/fd/1" >> /etc/cron.d/foreman-cleaner
+echo "30 * * * * root . /root/envs.sh; /usr/bin/python -W ignore /install/check_windows.py check-join -c /install/config.yaml > /proc/1/fd/1" >> /etc/cron.d/foreman-cleaner
 service cron restart && tail -f /var/log/cron.log
